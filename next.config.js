1e4f7021683ab10fd6e19ab04bf3134f2bb62f59
Janiel Banasihan <janimargaret09@gmail.com>
2021-07-04 15:34:46 +0800

    added style to sign in and sign up page

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    wpURL: "http://wpecommerce.local",
+    consumerKey: "ck_7d7bda92f5ef41b3d0aa2619a790b02344af2e27",
+    consumerSecret: "cs_51e665d38dba7968515bc789694aa286bdb0337c",
+    sessionSecret: "5EBE2294ECD0E0F08EAB7690D2A6EE69",
+  },
+};
